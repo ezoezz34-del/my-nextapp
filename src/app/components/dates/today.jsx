5cc5948cbd63879c09ed8d2d1f32816a84bca166
unknown <ezzeldineehab@gmail.com>
2026-06-18 05:31:06 +0300

    update

--- src/app/components/dates/today.jsx
+++ src/app/components/dates/today.jsx
@@ -39,7 +39,7 @@ export default function Yesterday() {
         <span className="score">0</span>
 
         <div className="matdel">
-          <h5>لم تبدأ بعد</h5>
+          <h5> تبدأ المباراه</h5>
           <h5>05:00 Am</h5>
          <h5>كأس العالم - المجموعة ك</h5>
         </div>
