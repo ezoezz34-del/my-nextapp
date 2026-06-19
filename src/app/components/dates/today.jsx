d24265613ae0d1bc818c81b63e247d7e1cf5b025
unknown <ezzeldineehab@gmail.com>
2026-06-19 04:20:28 +0300

    update

--- src/app/components/dates/today.jsx
+++ src/app/components/dates/today.jsx
@@ -20,23 +20,21 @@ HT // هايتي
 export default function Yesterday() {
   return (
     <div className="table">
-        <Link className="maz" href="./live">
 
       <div className="matches">
         < QA  style={{ width: "60px" }} />
         <span className="score">0</span>
 
         <div className="matdel">
-   <h5>لم تبدأ بعد</h5>
-          <h5>01:00 Am</h5>
-         <h5>كأس العالم - المجموعة ب</h5>        </div>
+                 <h3>انتهت المباراة</h3>
+        
+          </div>
 
-        <span className="score">0</span>
+        <span className="score">6</span>
         < CA style={{ width: "60px" }} />
       </div>
-              </Link>
 
-        <Link className="maz" href="">
+        <Link className="maz" href="./live">
 
       <div className="matches">
 
