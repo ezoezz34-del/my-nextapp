3cdd8f80d8de0955e93c72ec3899400a104c64f8
unknown <ezzeldineehab@gmail.com>
2026-07-09 20:55:18 +0300

    update

--- src/app/components/dates/today.jsx
+++ src/app/components/dates/today.jsx
@@ -84,7 +84,7 @@ export default function Yesterday() {
 
  */}
 {/* Morocco vs France */}
-  <Link className="maz" href="">
+  <Link className="maz" href="./live">
     <div className="matches">
       <div className="team">
         <MA className="flag" />
@@ -94,7 +94,7 @@ export default function Yesterday() {
       <span className="score">0</span>
 
       <div className="matdel">
-        <h5>لم تبدأ بعد</h5>
+        <h4> تبدأ المباراه</h4>
         <h5>11:00 Pm</h5>
         <h5>ربع النهائي</h5>
       </div>
