bb8802b979eff2f23e471762d049ffde12661e60
unknown <ezzeldineehab@gmail.com>
2026-07-16 18:14:05 +0300

    update

--- src/app/components/dates/today.jsx
+++ src/app/components/dates/today.jsx
@@ -27,25 +27,22 @@ import Image from "next/image";
 export default function Yesterday() {
   return (
     <div className="table">
-                    <Link className="maz" href="./live">
       <div className="matches">
 <div className="team">
     <GB className="flag" />
 
     <span className="teamName">England</span>
   </div>     
-   <span className="score">0</span>
+   <span className="score">1</span>
  <div className="matdel">
- <h4>  تبدأ المباراه </h4>
-        <h5>10:00 Pm</h5>
-        <h5>نصف النهائي</h5>
+   <h3>انتهت المباراة</h3>         
+
         </div>
-        <span className="score">0</span>
+        <span className="score">2</span>
 <div className="team">
     <AR className="flag" />
     <span className="teamName">Argentina</span>
   </div>      </div>
-        </Link>  
       <div className="matches">
 <div className="team">
     <ES className="flag" /> 
